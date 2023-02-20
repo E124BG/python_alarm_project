28dafd17b430876f77100d14a2ab31ce3dfef28c
E124BG <graak@live.fr>
2023-02-20 19:32:38 +0100

    padded hour, minutes in 00:00 format

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def alarm(seconds):
         #compute remaining minutes and seconds
         
         remaining_time = seconds - time_elapsed
-        remaining_minutes = remaining_time//60
-        remaining_seconds = remaining_time%60
-        print("{}, {}".format(remaining_minutes, remaining_seconds))
+        remaining_minutes = remaining_time//60 # integer division
+        remaining_seconds = remaining_time%60 # remainder of euclidean division
+        print("{:02d}: {:02d}".format(remaining_minutes, remaining_seconds)) # :02d pads the var to be length 2 digits with 0's
         
     playsound("alarm-flute.mp3")
 
         
-alarm(110)
+alarm(5)
